387fe268460bed9736af839c9a270e3015fdf4f4
Pablo Alvarez <pipualvarez1@gmail.com>
2024-03-08 20:20:51 -0300

    layout header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-import Nav from './Nav'
 import Logo from '/public/logo.svg'
 import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='py-5 flex justify-center'>
+    <div className='py-5 flex justify-center shadow-lg shadow-[#ffffff21]'>
       <section className='w-[5rem] cursor-pointer'>
         <Link href={'/'}>
           <Image src={Logo} alt='logo' />
